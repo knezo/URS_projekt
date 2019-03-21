98419a2a26dad6b56ac4c570559f1266b4bc74c5
Matija Kelemen <matijakelemen3@gmail.com>
2019-03-21 16:47:17 +0100

    Added timeout for Response

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /*
- * URS_projekt
+ * URS_projekt - Soil moisture measure
  *
  * Created: 18/03/2019 17:18:29
  * Author : Kristijan, Bruno, Matija
@@ -7,14 +7,20 @@
 
 #define F_CPU 8000000UL
 
+//#include <stdio.h>
+
 #include <avr/io.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+//#include <string.h>
+#include <util/delay.h>
+
 #include "lcd.h"
-#include "util/delay.h"
+
 #define DHT11_PIN 7
-uint8_t c=0,I_RH,D_RH,I_Temp,D_Temp,CheckSum;
+#define timeout 200
+
+uint8_t c = 0, I_RH, D_RH, I_Temp, D_Temp, CheckSum;
+char data[8];
 
 void Request()				/* Microcontroller send start pulse/request */
 {
@@ -24,25 +30,31 @@ void Request()				/* Microcontroller send start pulse/request */
 	PORTA |= (1<<DHT11_PIN);	/* set to high pin */
 }
 
-void Response()				/* receive response from DHT11 */
-{
-	DDRA &= ~(1<<DHT11_PIN);
-	while(PINA & (1<<DHT11_PIN));
-	while((PINA & (1<<DHT11_PIN))==0);
-	while(PINA & (1<<DHT11_PIN));
+void Response()	{			/* receive response from DHT11 */
+	DDRA &= ~ _BV(DHT11_PIN);
+	
+	uint8_t timeo = timeout;
+	while(PINA & _BV(DHT11_PIN) && timeo--);
+	
+	timeo = timeout;
+	while(!(PINA & _BV(DHT11_PIN)) && timeo--);
+
+	timeo = timeout;
+	while(PINA & _BV(DHT11_PIN) && timeo--);
 }
 
-uint8_t Receive_data()			/* receive data */
-{
-	for (int q=0; q<8; q++)
-	{
-		while((PINA & (1<<DHT11_PIN)) == 0);  /* check received bit 0 or 1 */
+uint8_t Receive_data() { /* receive data */
+	uint8_t timeo = timeout;
+	
+	for (int q=0; q<8; q++)	{
+		while(((PINA & (1<<DHT11_PIN)) == 0) && timeo--);  /* check received bit 0 or 1 */
 		_delay_us(30);
 		if(PINA & (1<<DHT11_PIN))/* if high pulse is greater than 30ms */
 		c = (c<<1)|(0x01);	/* then its logic HIGH */
 		else			/* otherwise its logic LOW */
 		c = (c<<1);
-		while(PINA & (1<<DHT11_PIN));
+		timeo = timeout;
+		while((PINA & (1<<DHT11_PIN)) && timeo--);
 	}
 	return c;
 }
@@ -64,13 +76,31 @@ int ADC_Read()
 	return(ADC);		/* return the ADCW */
 }
 
-
-	char data[5];
-
-
-int main(void)
-{
+void lcd_print(uint8_t I_RH, uint8_t I_Temp, float moisture) {
+	lcd_clrscr();
 	
+	lcd_gotoxy(0, 0);
+	lcd_puts("H: ");
+	itoa(I_RH, data, 10);
+	lcd_puts(data);
+	lcd_puts(" %");
+	
+	lcd_gotoxy(9, 0);
+	lcd_puts("T: ");
+	itoa(I_Temp, data, 10);
+	lcd_puts(data);
+	lcd_putc(0xDF);
+	lcd_puts("C");
+	
+	lcd_gotoxy(0, 1);
+	lcd_puts("M: ");
+	dtostrf(moisture,3,2,data);
+	lcd_puts(data);
+	lcd_puts(" %");
+}
+
+
+int main(void){
 	
 	DDRD = _BV(4);
 
@@ -78,77 +108,41 @@ int main(void)
 	TCCR1B = _BV(WGM12) | _BV(CS11);
 	OCR1B = 80;
 	
-
 	lcd_init(LCD_DISP_ON);			/* Initialize LCD */
-	lcd_clrscr();			/* Clear LCD */
-	lcd_gotoxy(0,0);		/* Enter column and row position */
-	lcd_puts("H =");
-	lcd_gotoxy(0,1);
-	lcd_puts("T = ");
-
 
 	ADC_Init();		/* initialize the ADC */
-	char array[10];
+	
 	int adc_value;
 	float moisture;
-
-	//_delay_ms(100);
-
-	while(1)
-	{	
+	
+	while(1) {	
 		
-		Request();		/* send start pulse */
-		Response();		/* receive response */
-		I_RH=Receive_data();	/* store first eight bit in I_RH */
-		D_RH=Receive_data();	/* store next eight bit in D_RH */
-		I_Temp=Receive_data();	/* store next eight bit in I_Temp */
-		D_Temp=Receive_data();	/* store next eight bit in D_Temp */
-		CheckSum=Receive_data();/* store next eight bit in CheckSum */
+		Request();				// send start pulse 
+		Response();				// receive response 
+		I_RH=Receive_data();	// store first eight bit in I_RH 
+		D_RH=Receive_data();	// store next eight bit in D_RH 
+		I_Temp=Receive_data();	// store next eight bit in I_Temp 
+		D_Temp=Receive_data();	// store next eight bit in D_Temp 
+		CheckSum=Receive_data();// store next eight bit in CheckSum 
 		
-		if ((I_RH + D_RH + I_Temp + D_Temp) != CheckSum)
-		{
-			lcd_gotoxy(0,0);
-			lcd_puts("Error");
-		}
-		
-		else
-		{
-			itoa(I_RH,data,10);
-			lcd_gotoxy(4,0);
-			lcd_puts(data);
-			lcd_puts("%");
-
-			itoa(I_Temp,data,10);
-			lcd_gotoxy(4,1);
-			lcd_puts(data);
-			lcd_putc(0xDF);
-			lcd_puts("C ");
-			
-			//itoa(checksum,data,10);
-			//lcd_puts(data);
-			//lcd_puts("b");
-
-
-			// SOIL
-
-			//lcd_gotoxy(9,0);
-			//lcd_puts("H = 00%");
-
-
-		}
-
 		adc_value = ADC_Read();	/* Copy the ADC value */
 		moisture = 100-(adc_value*100.00)/1023.00; /* Calculate moisture in % */
-		lcd_gotoxy(9,0);
-		lcd_puts("M: ");
-		dtostrf(moisture,3,2,array);
-		strcat(array,"%   ");	/* Concatenate unit of % */
-		lcd_gotoxy(9,1);	/* set column and row */
-		lcd_puts(array);	/* Print moisture on second row */
-		memset(array,0,10);
-		_delay_ms(500);
 		
-		//_delay_ms(500);
+		if ((I_RH + D_RH + I_Temp + D_Temp) != CheckSum) {
+			lcd_clrscr();
+			lcd_gotoxy(0, 0);
+			lcd_puts("Connection error");
+			lcd_gotoxy(0, 1);
+			lcd_puts("M: ");
+			dtostrf(moisture,3,2,data);
+			lcd_puts(data);
+			_delay_ms(500);
+			continue;
+		} else {
+			lcd_print(I_RH, I_Temp, moisture);
+			_delay_ms(500);
+		}
+		
 	}
 
 }
